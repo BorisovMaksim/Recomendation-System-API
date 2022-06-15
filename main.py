049092a4fe75ff7bf05588a04d67dbce5bb8edce
BorisovMaksim <maksim.borisov.2013@gmail.com>
2022-06-15 20:38:16 +0300

    init

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+import json
+import psycopg2
+import os
+
+
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-class status (Resource):
+parser = reqparse.RequestParser()
+parser.add_argument('tracks', type=list, location='form')
+parser.add_argument('n', type=int, location='form')
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+con = psycopg2.connect(DATABASE_URL)
+
+
+
+class Playlist(Resource):
     def get(self):
-        try:
-            return {'data': 'Api is Running'}
-        except:
-            return {'data': 'An Error Occurred during fetching Api'}
+        return """You can use POST for track recommendation! For example """
+
+    def post(self):
+        args = parser.parse_args()
+        tracks_uri = json.loads("".join(args['tracks']))
+        cur = con.cursor()
+
+        # similar_tracks = system.model.predict(tracks_uri=tracks_uri, n=args['n'])
+        return tracks_uri.to_json()
 
 
-
-class Sum(Resource):
-    def get(self, a, b):
-        return jsonify({'data': a+b})
-
-
-api.add_resource(status, '/')
-api.add_resource(Sum, '/add/<int:a>,<int:b>')
-
+api.add_resource(Playlist, '/')
 if __name__ == '__main__':
     app.run()
